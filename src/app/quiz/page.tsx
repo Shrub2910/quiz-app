28eea7270954c92f36e9ba85b59b21ae8f30386a
Shrub2910 <isaacmcallister2006@gmail.com>
2025-06-25 18:24:21 +0100

    Added score count and button to return to homepage

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState, useRef } from "react";
+import Link from "next/link";
 
 
 type Question = {
@@ -13,6 +14,7 @@ export default function Quiz() {
     const [questions, setQuestions] = useState<Question[]>([])
     const [loading, setLoading] = useState(true)
     const [questionNumber, setQuestionNumber] = useState(0)
+    const [questionsCorrect, setQuestionsCorrect] = useState(0)
 
     const has_fetched = useRef(false)
 
@@ -41,16 +43,21 @@ export default function Quiz() {
 
     if (loading || !questions) return <p>Questions are loading...</p>
 
-    const currentQuestion = questions[questionNumber]
+    let currentQuestion: Question
+    let answers: string[] = []
 
-    const answers = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer]
+    if (questionNumber <= 9) {
+        currentQuestion = questions[questionNumber]
 
-    for (let i = 0; i <= 3; i++) {
-        const randomNumber = Math.floor(Math.random() * 4)
+        answers = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer]
 
-        const temp = answers[i]
-        answers[i] = answers[randomNumber]
-        answers[randomNumber] = temp
+        for (let i = 0; i <= 3; i++) {
+            const randomNumber = Math.floor(Math.random() * 4)
+
+            const temp = answers[i]
+            answers[i] = answers[randomNumber]
+            answers[randomNumber] = temp
+        }
     }
 
     return (
@@ -61,11 +68,26 @@ export default function Quiz() {
 
                 <ul>
                     {answers.map((answer, i) => (
-                        <li className="px-2 py-3 my-6 rounded-md text-3xl text-center text-white bg-blue-600 hover:bg-orange-600 transition" key={i} onClick={() => {setQuestionNumber(questionNumber + 1)}}>{answer}</li>
+                        <li className="px-2 py-3 my-6 rounded-md text-3xl text-center text-white bg-blue-600 hover:bg-orange-600 transition" key={i} onClick={() => {
+                            setQuestionNumber(questionNumber + 1)
+                            if (answer === questions[questionNumber].correct_answer) {
+                                setQuestionsCorrect(questionsCorrect + 1)
+                            }
+                        }}>{answer}</li>
                     ))}
                 </ul>
+
+                <p>Questions correct so far: {questionsCorrect}</p>
                 </div>
-            ): <h1>End of quiz</h1>
+            ): (
+                <div className="flex min-h-screen items-center bg-white flex-col">
+                    <h1 className="text-5xl my-6">Congratulations!</h1>
+                    <p className="text-2xl"> You got {questionsCorrect} questions right!</p>
+                    <Link href="/">
+        <               button className="px-2 py-3 my-6 rounded-md bg-blue-600 text-white hover:bg-orange-600 transition">Return to home page!</button>
+                    </Link>  
+                </div>
+            )
             }
         </main>
     );
